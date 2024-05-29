4ab642ac8fb41f5c371ed10a0b05d2dafe41d305
João Matheus <138176279+joaomatheusm@users.noreply.github.com>
2024-05-29 12:36:49 -0300

    comments removed

--- js/script.js
+++ js/script.js
@@ -4,14 +4,11 @@ const closeMenu = document.querySelector('#close-menu');
 
 openMenu.addEventListener('click', () => {
     menu.style.display = 'flex';
-    // Define a posição inicial do menu à direita fora da tela (largura do menu vezes -1)
     menu.style.right = (menu.offsetWidth * -1) + 'px';
 
-    // Define um temporizador para a transição suave do menu
     setTimeout(() => {
         menu.style.opacity = '1';
 
-        // Define a posição do menu como 0 pixels da borda direita (totalmente visível)
         menu.style.right = '0';
         openMenu.style.display = 'none';
     }, 10);
